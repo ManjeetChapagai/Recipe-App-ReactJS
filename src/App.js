309718fc09d478aba8347179fcde2031d4895521
ManjeetChapagai <74332423+ManjeetChapagai@users.noreply.github.com>
2021-03-07 07:10:03 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 const App = () => {
+  const APP_ID = "30d2cea4";
+  const APP_KEY = "46666b757c73f2cf64d838ad0f6eb57f";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
+  };
+
   return (
-    <div>
-      <h1> I am a Recipe Finder App</h1>
+    <div className="App">
+      <form className="form">
+        <input className="search-form" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
